4895b3c86b146781bfb0a6c7fdc7750ae1e73fdd
Luxzinho244 <144269204+Luxzinho244@users.noreply.github.com>
2026-02-17 17:37:39 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Luxzinho244/RYUZEN/main/loader.lua"))()
+-- GROK x LUIS LOADER | Crimson Night / Grok Hub | Blox Fruits 2026
+-- Loader simples pro Auto Farm Hub completo
+
+print("Loader iniciado! Carregando Grok x Luis Hub... 🚀")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Luxzinho244/Crimson-night/refs/heads/main/Main.lua", true))()
+
+print("Hub carregado! Bora farmar level, mlk! 🍌")
